9fb15b04de73c6c8f0e36b161c344c6737012b4d
JaydeepKachare <jaydeepmea2@gmail.com>
2021-03-06 22:00:52 +0530

    Assignment 7 completed

--- Assignment7/Assignment7_3.py
+++ Assignment7/Assignment7_3.py
@@ -6,7 +6,6 @@ class Numbers :
 class Arithmetic : 
     def __init__(self,Value):
         self.Value = Value 
-    
 
     def ChkPrime(self) : 
         for i in range(2,self.Value) : 
@@ -14,28 +13,25 @@ class Arithmetic :
                 return False 
         return True 
 
-
     def ChkPerfect(self) : 
-        if self.Value == self.SumFactors() :
+        if self.Value == self.SumFactors(self.Value) :
             return True
         else : 
             return False 
 
-
     def Factors(self) : 
         for i in range(1,self.Value) : 
             if self.Value%i == 0 : 
-                print(i) 
+                print(i, end=" ") 
+        print() 
 
-
-    def SumFactors(self) : 
+    @staticmethod
+    def SumFactors(Value) : 
         sum = 0 
-        for i in range(1,self.Value) : 
-            if self.Value%i == 0 : 
+        for i in range(1,Value) : 
+            if Value%i == 0 : 
                 sum += i 
         return sum  
-
-
 
 
 
@@ -54,6 +50,8 @@ def main() :
         print("Number is perfect")
     else : 
         print("number is not perfect ")
+
+
 
 if __name__ == "__main__" : 
     main() 
